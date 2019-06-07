b6f1f65ffb4b72fa94170180531325e3d9440bab
Miroslav Savovski <miroslav.savovski@scalefocus.com>
2019-06-07 17:23:40 +0300

    FooterTabs - refactoring.

---
+++ src/common/footerLinks.tsx
@@ -0,0 +1,19 @@
+import { Link } from './interfaces';
+
+export const footerLinks: Link[] = [
+  {
+    link: 'Dashboard',
+    routerLink: '/dashboard',
+    icon: 'web'
+  },
+  {
+    link: 'Forms',
+    routerLink: '/users',
+    icon: 'not_listed_location'
+  },
+  {
+    link: 'Message',
+    routerLink: '/message',
+    icon: 'message'
+  },
+];
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
-import {Hidden } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Hidden, BottomNavigation, BottomNavigationAction, Icon } from '@material-ui/core';
 
-import FooterTabs from './FooterTabs';
+import { footerLinks } from '../../common/footerLinks';
 
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    stickToBottom: {
+      width: '100%',
+      position: 'fixed',
+      bottom: 0,
+      left: 0
+    }
+  }),
+);
 
 const Footer: React.FC = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(null);
+
   return (
     <>
       <Hidden only={['sm', 'md', 'lg', 'xl']}>
-        <FooterTabs />
+        <div className={classes.stickToBottom}>
+          <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            showLabels
+          >
+            {footerLinks.map((item) => (
+              <BottomNavigationAction key={item.link} label={item.link} icon={<Icon>{item.icon}</Icon>} />
+            ))}
+          </BottomNavigation>
+        </div >
+        );
       </Hidden>
     </>
   );
 }
-  
+
 export default Footer;
